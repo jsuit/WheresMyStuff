9c26c27ebcacdc5fe190699820f7af9e1ab4d40f
Jonathan <jonathan.suit@gmail.com>
2013-03-06 17:51:40 -0500

    Revert "added more methods to db"
    This reverts commit aaeee4bd7e41dbe469a2f52f9d63b1804280f931.

--- src/com/example/wheresmystuff/Model/Admin.java
+++ src/com/example/wheresmystuff/Model/Admin.java
@@ -2,13 +2,4 @@ package com.example.wheresmystuff.Model;
 
 public class Admin extends RegularUser {
 
-	public Admin(String email, String name, String password, String phoneNum,
-			String zip, String street, int login_attempt) {
-		super(email, name, password, phoneNum, zip, street, login_attempt);
-		// TODO Auto-generated constructor stub
-	
-	}
-	
-	
-
 }
