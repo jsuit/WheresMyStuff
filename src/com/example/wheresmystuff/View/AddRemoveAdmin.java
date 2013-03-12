11d6f339b642f1c1d340eb495be9f061501cebed
Jonathan <jonathan.suit@gmail.com>
2013-03-12 19:05:32 -0400

    final touches

--- src/com/example/wheresmystuff/View/AddRemoveAdmin.java
+++ src/com/example/wheresmystuff/View/AddRemoveAdmin.java
@@ -1,11 +1,45 @@
 package com.example.wheresmystuff.View;
 
-import android.app.Activity;
+import com.example.wheresmystuff.R;
+import com.example.wheresmystuff.Model.DB;
+import com.example.wheresmystuff.Presenter.AddAdminPresenter;
+import com.example.wheresmystuff.R.id;
+import com.example.wheresmystuff.R.layout;
+import com.example.wheresmystuff.R.menu;
+
 import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddRemoveAdmin extends Activity {
 
-	public void onCreate(Bundle savedInstanceBundle){
-		 super.onCreate(savedInstanceBundle);
+	private AddAdminPresenter my_presenter;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_remove_admin);
+		my_presenter = new AddAdminPresenter(this, new DB(this));
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.add_remove_admin, menu);
+		return true;
+	}
+	
+	public void AddAdmin(View createAdminButton) {
+		
+		String user_name = ((EditText) findViewById(R.id.adminID)).getText().toString();
+		String email_add = ((EditText) findViewById(R.id.email_address)).getText().toString();
+		String password = ((EditText) findViewById(R.id.password)).getText().toString();
+		String retype_password = ((EditText) findViewById(R.id.retype_password)).getText().toString();
+		Log.d("AddRemoveAdmin", "Create admin button clicked");
+		
+	}
+
 }
