4649e73ae20b24e50cd96cdd845fc41cc4bbf0a4
Jonathan <jonathan.suit@gmail.com>
2013-04-08 14:30:47 -0400

    Added some more user feedback when logging in

--- src/com/example/wheresmystuff/validation/ValidatePassword.java
+++ src/com/example/wheresmystuff/validation/ValidatePassword.java
@@ -5,13 +5,16 @@ public class ValidatePassword {
 public static String validate(String password, String check_password){
 
 		if (password == null || check_password == null || "".compareTo(password) == 0) {
-			return "Invalid Password\n";
+			return "Invalid Password. Need a Password\n";
 		}
 		if(password.compareTo(check_password) != 0){
-			return "Invalid Password\n";
+			return "Invalid Password. Passwords don't match.\n";
 		}
-		if(password.length() < 6 || password.length() > 100){
-			return "Invalid Password\n";
+		if(password.length() < 6){
+			return "Invalid Password. Need 7 characters with at least one number\n";
+		}
+		if(password.length()>99){
+			return "Invalid Password. Too Long.\n";
 		}
 		int numbers = 0;
 		for (int i = 0; i < password.length(); i++) {
@@ -23,7 +26,14 @@ public static String validate(String password, String check_password){
 		if(numbers < password.length() && numbers >= 1){
 			return null;
 
-		}else return "Invalid Password\n";
+		}
+		if(numbers == password.length()){
+			return "Invalid Password. Can't be all numbers.\n";
+		}
+		if(numbers == 0 ){
+			return "Invalid Password. No Numbers.";
+		}
+		else return "Invalid Password.\n";
 
 		//boolean value = password.matches("[((0-9)|(a-z)|(A-Z)){6,12}]");
 		//return true;
