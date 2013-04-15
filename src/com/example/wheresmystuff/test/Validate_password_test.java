75c1dba9868d2488614094f1ac3ea909bc11e919
Jonathan <jonathan.suit@gmail.com>
2013-04-15 00:29:41 -0400

    committ new

---
+++ src/com/example/wheresmystuff/test/Validate_password_test.java
@@ -0,0 +1,10 @@
+package com.example.wheresmystuff.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.wheresmystuff.validation.ValidatePassword;
+
+
